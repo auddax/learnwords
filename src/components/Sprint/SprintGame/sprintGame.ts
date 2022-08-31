9565a9e8a7b9a224ccf72090ddf8e2d1f2e8a3e6
Kirill Konovalov <auddax@gmail.com>
2022-08-31 15:55:25 +0300

    fix: add check for accuracy zero value in progressBar function (Wed, Aug 31, 2022 03:55:25 PM)

--- src/components/Sprint/SprintGame/sprintGame.ts
+++ src/components/Sprint/SprintGame/sprintGame.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 /* eslint-disable no-console */
 /* eslint-disable class-methods-use-this */
 import environment from '../../../environment/environment';
@@ -134,6 +135,19 @@ class SprintGame implements ISprintGame {
       main.innerHTML = `
         <section class="sprint-game">
           <div class="sprint-game__card">
+            <div class="sprint-game__answers">
+              <div class="answers__check">
+                <svg class="check" viewBox="0 0 24 24">
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path>
+                </svg>
+                <svg class="check" viewBox="0 0 24 24">
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path>
+                </svg>
+                <svg class="check" viewBox="0 0 24 24">
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path>
+                </svg>
+              </div>
+            </div>
             <div class="sprint-game__timer">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M22 5.7l-4.6-3.9-1.3 1.5 4.6 3.9L22 5.7zM7.9 3.4L6.6 1.9 2 5.7l1.3 1.5 4.6-3.8zM12.5 8H11v6l4.7 2.9.8-1.2-4-2.4V8zM12 4c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7z"/>
