2346b3eb53d82c381248629b7d4e1ea88887946d
Kirill Konovalov <auddax@gmail.com>
2022-09-04 14:38:02 +0300

    feat: add authorization check in PageHeader component (Sun, Sep 04, 2022 02:38:02 PM)

--- src/components/PageHeader/pageHeader.ts
+++ src/components/PageHeader/pageHeader.ts
@@ -4,8 +4,22 @@ import { IPageHeader } from '../../types/interfaces';
 import './pageHeader.scss';
 
 class PageHeader implements IPageHeader {
+  userName: string | null;
+
+  constructor() {
+    this.userName = localStorage.getItem('userName');
+  }
+
   render() {
     const header = document.querySelector('.page-header');
+    const menuSignIn = `
+      <button class="menu__button-signin" id="${View.SIGNIN}">Sign in</button>
+    `;
+    const menuSignOut = `
+      <span class="menu__username">${this.userName}</span>
+      <button class="menu__button-signout" id="signout">Sign out</button>
+    `;
+
     if (header) {
       header.innerHTML = `
       <div class="header container">
@@ -17,9 +31,7 @@ class PageHeader implements IPageHeader {
           <li class="menu-item" id="${View.DICTIONARY}">Dictionary</li>
           <li class="menu-item" id="${View.GAMES}">Games</li>
           <li class="menu-item" id="${View.STATISTICS}">Statistics</li>
-          <li class="menu-item">
-            <button class="menu__button-signin" id="${View.SIGNIN}">Sign in</button>
-          </li>
+          <li class="menu-item">${this.userName ? menuSignOut : menuSignIn}</li>
         </ul>
       </div>
       `;
